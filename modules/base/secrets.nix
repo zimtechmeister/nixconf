0c3e9b4bddc550a474bb5ed17a29330214eed7f7
Tim Zechmeister <tim.zechmeister03@gmail.com>
2026-07-14 00:08:44 +0200

    bug fix

--- modules/base/secrets.nix
+++ modules/base/secrets.nix
@@ -5,9 +5,9 @@
     ];
 
     age.secrets = {
-      root-password.file = ../../_secrets/root-password.age;
-      tim-password.file = ../../_secrets/tim-password.age;
-      disk-encryption.file = ../../_secrets/disk-encryption.age;
+      root-password.file = ../../secrets/root-password.age;
+      tim-password.file = ../../secrets/tim-password.age;
+      disk-encryption.file = ../../secrets/disk-encryption.age;
     };
     environment.systemPackages = [
       inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default
